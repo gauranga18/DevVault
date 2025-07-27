1139d608a78a090f0976966acdddbe5abf7d186a
Saurav <sauravjyotikalita56@gmail.com>
2025-07-27 16:13:04 +0530

    Spring Configuration

---
+++ E2EE/src/main/java/com/JPrjkt/E2EE/E2EEApplication.java
@@ -0,0 +1,13 @@
+package com.JPrjkt.E2EE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2EEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2EEApplication.class, args);
+	}
+
+}
