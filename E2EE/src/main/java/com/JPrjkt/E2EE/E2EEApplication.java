f740d0674f856738bbee6218f0a3c4918a331b65
Saurav <sauravjyotikalita56@gmail.com>
2025-07-29 17:56:27 +0530

    Fixed the app running issue

--- E2EE/src/main/java/com/JPrjkt/E2EE/E2EEApplication.java
+++ E2EE/src/main/java/com/JPrjkt/E2EE/E2EEApplication.java
@@ -2,8 +2,9 @@ package com.JPrjkt.E2EE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class E2EEApplication {
 
 	public static void main(String[] args) {
