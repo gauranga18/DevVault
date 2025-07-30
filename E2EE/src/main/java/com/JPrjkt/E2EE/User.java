6e1e3278e546aff9b24ec2001c8dcd5b1e051352
Saurav <sauravjyotikalita56@gmail.com>
2025-07-30 13:20:47 +0530

    Fixed issue

--- E2EE/src/main/java/com/JPrjkt/E2EE/User.java
+++ E2EE/src/main/java/com/JPrjkt/E2EE/User.java
@@ -1,12 +1,17 @@
+package com.JPrjkt.E2EE;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data  // This generates getters, setters, toString, equals, hashCode
-@NoArgsConstructor // Default constructor
+ // Default constructor
 @AllArgsConstructor // Constructor with all fields (optional, for testing or DTOs)
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,7 +20,9 @@ public class User {
 
     @Column(unique = true)
     private String email;
+        public User() {
 
+        }
     private String password;
 
     private String role = "USER";
