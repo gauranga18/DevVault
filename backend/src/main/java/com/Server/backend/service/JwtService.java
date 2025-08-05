fbdbd343df8b8aa78a277981d0865eaae6bc7333
Saurav <sauravjyotikalita56@gmail.com>
2025-08-05 19:41:30 +0530

    Changes in JWT configs in service

--- backend/src/main/java/com/Server/backend/service/JwtService.java
+++ backend/src/main/java/com/Server/backend/service/JwtService.java
@@ -1,7 +1,15 @@
 package com.Server.backend.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+@Value("${security.jwt.secret-key}")
+    private String secretKey;
+@Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+public String extractUsername(String token){
+    return extractClaim(token, Claims::getSubject);
+}
 }
