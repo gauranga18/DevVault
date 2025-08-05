0b2109ad1154423a268c146879df82a07f7e0d84
Saurav <sauravjyotikalita56@gmail.com>
2025-08-05 16:30:29 +0530

    Changes in user Entity

--- backend/src/main/java/com/Server/backend/model/User.java
+++ backend/src/main/java/com/Server/backend/model/User.java
@@ -1,4 +1,43 @@
 package com.Server.backend.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.naming.Name;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    public User() {
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
 }
