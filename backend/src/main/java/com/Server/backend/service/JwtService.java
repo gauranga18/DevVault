b1dd2d0ec6437c469bf1bcfc256b03b1c15d1472
Saurav <sauravjyotikalita56@gmail.com>
2025-08-07 19:22:13 +0530

    Added return functions

--- backend/src/main/java/com/Server/backend/service/JwtService.java
+++ backend/src/main/java/com/Server/backend/service/JwtService.java
@@ -1,7 +1,11 @@
 package com.Server.backend.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Map;
 
 @Service
 public class JwtService {
@@ -15,5 +19,28 @@ public String extractUsername(String token){
 public<T> T extractClaim(String token, Function<Claims, T> claimsResolver){
     final Claims claims = extractAllClaims(token);
     return claimsResolver.apply(claims);
+}
+public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+    return buildToken(extraClaims, userDetails, jwtExpiration);
+}
+public long getExpirationTime(){
+    return jwtExpiration;
+}
+private String buildToken(
+        Map<String, Object> extraClaims,
+        UserDetails userDetails,
+        long expiration
+){
+    return JWTs
+            .builder()
+            .setClaims(extraClaims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date(system.currentTimeMills()))
+            .setExpiration(new Date(system.currentTimeInMills()))
+            .signWith(getSignInKey(), signatureAlgorithm.ES256)
+            .compact();
+}
+public boolean isTokenValid(){
+
 }
 }
