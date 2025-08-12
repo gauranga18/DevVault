197260626b8f25cb5013a0f7acc7ca6fe8677b10
Saurav <sauravjyotikalita56@gmail.com>
2025-08-13 02:20:08 +0530

    Added dependency

--- backend/src/main/java/com/Server/backend/service/JwtService.java
+++ backend/src/main/java/com/Server/backend/service/JwtService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 
