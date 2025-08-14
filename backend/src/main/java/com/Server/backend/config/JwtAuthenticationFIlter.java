1c33b5c6085db823b082964ded52dcef85af9409
Saurav <sauravjyotikalita56@gmail.com>
2025-08-14 15:16:59 +0530

    Added authentication filter

--- backend/src/main/java/com/Server/backend/config/JwtAuthenticationFIlter.java
+++ backend/src/main/java/com/Server/backend/config/JwtAuthenticationFIlter.java
@@ -1,8 +1,33 @@
 package com.Server.backend.config;
 
+import com.Server.backend.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Component
 public class JwtAuthenticationFIlter {
-
+private final HandlerExceptionResolver handlerExceptionResolver;
+private final JwtService jwtService;
+private final UserDetailsService userDetailsService;
+public JwtAuthenticationFIlter(
+        JwtService jwtService,
+        UserDetailsService userDetailsService,
+        HandlerExceptionResolver handlerExceptionResolver){
+this.jwtService = jwtService;
+this.userDetailsService = userDetailsService;
+this.handlerExceptionResolver = handlerExceptionResolver;
+}
+@Override
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain
+        ){
+final String authHeader = request.getHeader("Authorization");
+}
 }
