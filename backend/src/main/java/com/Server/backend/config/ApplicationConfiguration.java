ec55a6e65f4e2c33733beb35bb39bf8c21a21f38
Saurav <sauravjyotikalita56@gmail.com>
2025-08-14 14:46:56 +0530

    Adding first bean at user repository

--- backend/src/main/java/com/Server/backend/config/ApplicationConfiguration.java
+++ backend/src/main/java/com/Server/backend/config/ApplicationConfiguration.java
@@ -1,4 +1,20 @@
 package com.Server.backend.config;
 
+import com.Server.backend.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
 public class ApplicationConfiguration {
+private final UserRepository userRepository;
+public ApplicationConfiguration(UserRepository userRepository){
+    this.userRepository = userRepository;
+}
+@Bean
+    UserDetailsService userDetailsService(){
+    return username -> userRepository.findByEmail(username)
+            .orElseThrow(()-> new UsernameNotFoundException("User Not Found"))
+}
 }
