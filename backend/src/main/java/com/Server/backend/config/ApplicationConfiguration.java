f550761d6841d645c4460b3c7f95af38cae9e620
Saurav <sauravjyotikalita56@gmail.com>
2025-08-14 14:56:44 +0530

    Complete Application Config

--- backend/src/main/java/com/Server/backend/config/ApplicationConfiguration.java
+++ backend/src/main/java/com/Server/backend/config/ApplicationConfiguration.java
@@ -3,8 +3,13 @@ package com.Server.backend.config;
 import com.Server.backend.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -16,5 +21,22 @@ public ApplicationConfiguration(UserRepository userRepository){
     UserDetailsService userDetailsService(){
     return username -> userRepository.findByEmail(username)
             .orElseThrow(()-> new UsernameNotFoundException("User Not Found"))
+}
+@Bean
+    BCryptPasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+}
+@Bean
+    
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config)throws Exception{
+    return config.getAuthenticationManager();
+}
+@Bean
+    AuthenticationProvider authenticationProvider(){
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(_);
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+
+    return authProvider;
 }
 }
