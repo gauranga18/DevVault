bed6a392578269c878ed89580a4c1d3f63e019a5
Saurav <sauravjyotikalita56@gmail.com>
2025-08-17 23:04:30 +0530

    Changes in Security Configuration

--- backend/src/main/java/com/Server/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/Server/backend/config/SecurityConfiguration.java
@@ -1,8 +1,13 @@
 package com.Server.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,5 +20,19 @@ public SecurityConfiguration(
 ){
     this.authenticationProvider = authenticationProvider;
     this.jwtAuthenticationFIlter = jwtAuthenticationFIlter;
+}
+@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws  Exception{
+    http
+            .csrf(csrf->csrf.disable())
+            .authorizeHttpRequests(authorize->authorize
+                    .requestMatchers("/auth/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .sessionManagement(session->session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFIlter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
 }
 }
