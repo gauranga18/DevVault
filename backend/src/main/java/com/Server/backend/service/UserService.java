0fe808203efaafc4d7d1f57923db61eea3ad08f0
Saurav <sauravjyotikalita56@gmail.com>
2025-08-23 01:48:02 +0530

    User Service Completed

---
+++ backend/src/main/java/com/Server/backend/service/UserService.java
@@ -0,0 +1,21 @@
+package com.Server.backend.service;
+
+import com.Server.backend.model.User;
+import com.Server.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+private final UserRepository userRepository;
+public UserService(UserRepository userRepository, EmailService emailService){
+    this.userRepository = userRepository;
+}
+public List<User> allUsers(){
+    List<User> users = new ArrayList<>();
+    userRepository.findAll().forEach(users::add);
+    return users;
+}
+}
