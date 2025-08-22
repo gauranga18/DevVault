d3e525097519ec8191b1c2e1415f1e4ce65f4c66
Saurav <sauravjyotikalita56@gmail.com>
2025-08-22 14:43:27 +0530

    Changes in Authentication Service

--- backend/src/main/java/com/Server/backend/service/AuthenticationService.java
+++ backend/src/main/java/com/Server/backend/service/AuthenticationService.java
@@ -5,6 +5,7 @@ import com.Server.backend.dto.RegisterUserDto;
 import com.Server.backend.dto.VerifyUserDto;
 import com.Server.backend.model.User;
 import com.Server.backend.repository.UserRepository;
+import jakarta.mail.MessagingException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class AuthenticationService {
@@ -70,5 +72,46 @@ public void verifyUser(VerifyUserDto input){
     }else {
         throw new RuntimeException("User Not Found");
     }
+}
+public void resendVerificationCode(String email){
+    Optional<User> optionalUser = userRepository.findByEmail(email);
+    if(optionalUser.isPresent()){
+        User user = optionalUser.get();
+        if (user.isEnabled()){
+            throw new RuntimeException("Account is already verified");
+        }
+        user.setVerificationCode(generateVerificationCode());
+        user.setVerificationCodeExpiresAt(LocalDateTime.now().plusHours(1));
+        sendVerificationEmail(user);
+        userRepository.save(user);
+    }else {
+        throw new RuntimeException("User Not Found");
+    }
+}
+public void sendVerificationEmail(User user){
+    String subject = "Account Verification";
+    String verificationCode = user.getVerificationCode();
+    String htmlMessage = "<html>"
+            + "<body style=\"font-family: Arial, sans-serif;\">"
+            + "<div style=\"background-color: #f5f5f5; padding: 20px;\">"
+            + "<h2 style=\"color: #333;\">Welcome to our app!</h2>"
+            + "<p style=\"font-size: 16px;\">Please enter the verification code below to continue:</p>"
+            + "<div style=\"background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0,0,0,0.1);\">"
+            + "<h3 style=\"color: #333;\">Verification Code:</h3>"
+            + "<p style=\"font-size: 18px; font-weight: bold; color: #007bff;\">" + verificationCode + "</p>"
+            + "</div>"
+            + "</div>"
+            + "</body>"
+            + "</html>";
+    try {
+        emailService.sendVerificationEmail(user.getEmail(), subject, htmlMessage);
+    } catch (MessagingException e){
+        e.printStackTrace();
+    }
+}
+private String generateVerificationCode(){
+    Random random = new Random();
+    int code = random.nextInt(900000)+100000;
+    return String.valueOf(code);
 }
 }
