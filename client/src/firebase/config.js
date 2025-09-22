bb16525d0fa8982728a097bc8d4dc9cbead7a6b9
Saurav <sauravjyotikalita56@gmail.com>
2025-09-22 12:20:26 +0530

    Changes in env configurations

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -1,21 +1,28 @@
+// config.js
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "REACT_APP_FIREBASE_API_KEY",
-  authDomain: "",
-  projectId: "devvault-73eeb",
-  storageBucket: "devvault-73eeb.firebasestorage.app",
-  messagingSenderId: "43041683609",
-  appId: "1:43041683609:web:360ffdd87d6f7b14de6008",
-  measurementId: "G-E4E2LLX2DL"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Setup Firestore + Auth (you’ll need them for DevVault)
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, analytics, db, auth };
